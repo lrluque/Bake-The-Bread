08c51dc042822966823a777ef1d63b1e1bfb7396
Lucas R. Luque <lucasrluque@icloud.com>
2021-11-14 12:09:35 +0100

    v.0.0.1

--- core/src/com/mygdx/game/BreadRain.java
+++ core/src/com/mygdx/game/BreadRain.java
@@ -18,9 +18,9 @@ import com.badlogic.gdx.utils.TimeUtils;
 import java.util.Iterator;
 
 public class BreadRain extends ApplicationAdapter {
-	private Texture breadImage, bucketImage, bucketImageSemiFull, bucketImageSemiFull2, bucketImageFull, liveImage, noLiveImage, backgroundTexture, planeImage1, planeImage2, planeImage3;
+	private Texture breadImage, bucketImage, bucketImageSemiFull, bucketImageSemiFull2, bucketImageFull, match, backgroundTexture, twolives, onelive, planeImage1, planeImage2, planeImage3;
 	private Sprite backgroundSprite;
-	private Sound bucketSound, liveSound, oven, liveLostSound;
+	private Sound bucketSound, liveSound, oven, liveLostSound, matchSound;
 	private Music backgroundMusic, fire;
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
@@ -31,7 +31,7 @@ public class BreadRain extends ApplicationAdapter {
 	private int liveCounter = 3, breadCounter = 0, backgroundCounter = 0, randomBackground, bucketCounter = 0, levelCounter = 0;
 	private double acceleration = 0.45, velocity = 6, xAcceleration = 0, friction = 0.025, breadVelocity = 2;
 	private String[] BucketSound = {"bucketSound", "bucketSound2", "bucketSound3"};
-	private Boolean planeActive = false;
+	private Boolean planeActive = false, matchBoolean = false, paused = false;
 
 	@Override
 	public void create () {
@@ -42,12 +42,12 @@ public class BreadRain extends ApplicationAdapter {
 		bucketImageSemiFull = new Texture(Gdx.files.internal("bucketsemifull.png"));
 		bucketImageSemiFull2 = new Texture(Gdx.files.internal("bucketsemifull2.png"));
 		bucketImageFull = new Texture(Gdx.files.internal("bucketfull.png"));
-		liveImage = new Texture(Gdx.files.internal("live.png"));
-		noLiveImage = new Texture(Gdx.files.internal("nolive.png"));
+		match = new Texture(Gdx.files.internal("match.png"));
 		backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("backgroundMusic.mp3"));
 		fire = Gdx.audio.newMusic(Gdx.files.internal("fire.mp3"));
 		oven = Gdx.audio.newSound(Gdx.files.internal("oven.mp3"));
 		liveSound = Gdx.audio.newSound(Gdx.files.internal("live.mp3"));
+		matchSound = Gdx.audio.newSound(Gdx.files.internal("matchSound.mp3"));
 		liveLostSound = Gdx.audio.newSound(Gdx.files.internal("liveLost.mp3"));
 		randomBackground = MathUtils.random(1, 4);
 		backgroundTexture = new Texture(Gdx.files.internal("background" + randomBackground + ".png"));
@@ -108,128 +108,146 @@ public class BreadRain extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		if (backgroundCounter > 6) {
-			randomBackground = MathUtils.random(1, 4);
-			backgroundCounter = 0;
-			backgroundTexture = new Texture(Gdx.files.internal("background" + randomBackground + ".png"));
-			backgroundSprite = new Sprite(backgroundTexture);
+		if (!paused) {
+			setScene();
+			batch.end();
+
+			if (Gdx.input.isKeyPressed(Input.Keys.LEFT) && !Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+				xAcceleration = -acceleration;
+			if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && !Gdx.input.isKeyPressed(Input.Keys.LEFT))
+				xAcceleration = acceleration;
+			if (!Gdx.input.isKeyPressed(Input.Keys.RIGHT) && !Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+				if (velocity > 0) {
+					xAcceleration = -0.02;
+				} else if (velocity < 0) {
+					xAcceleration = +0.02;
+				}
+			}
+			velocity += xAcceleration;
+			velocity *= 1 - friction;
+			bucket.x += velocity;
+
+			if (bucket.x < 0) bucket.x = 0;
+			if (bucket.x > 645) bucket.x = 645;
+
+			if (TimeUtils.nanoTime() - lastDropTime > 1900000000) spawnBread();
+			if (TimeUtils.nanoTime() - lastDropTimeLive > 1000000000) fallingLives();
+
+
+			for (Iterator<Rectangle> iter = raindrops.iterator(); iter.hasNext(); ) {
+				Rectangle raindrop = iter.next();
+				raindrop.y -= breadVelocity;
+				if (raindrop.y - 20 < 0) {
+					iter.remove();
+					liveLostSound.play();
+					liveCounter -= 1;
+					if (liveCounter == 0) {
+						Gdx.app.exit();
+					}
+				}
+				if (raindrop.overlaps(bucket)) {
+					if (bucketCounter < 10) {
+						bucketSound.play();
+						bucketCounter++;
+						iter.remove();
+					} else {
+						Gdx.app.exit();
+					}
+				}
+			}
+
+			for (Iterator<Rectangle> iter = lives.iterator(); iter.hasNext(); ) {
+				Rectangle live = iter.next();
+				live.y -= breadVelocity;
+				if (live.y - 20 < 0) {
+					iter.remove();
+				}
+				if (live.overlaps(bucket)) {
+					if (!matchBoolean) {
+						matchBoolean = true;
+					}
+					bucketSound.play();
+					iter.remove();
+				}
+			}
+			if (bucket.x == 645) {
+				if (matchBoolean && Gdx.input.isKeyPressed(Input.Keys.E) && liveCounter < 3) {
+					liveSound.play();
+					matchSound.play();
+					matchBoolean = false;
+					liveCounter++;
+				}
+			}
+			if (bucket.x == 645 && bucketCounter > 0) {
+				breadCounter += bucketCounter;
+				levelCounter += bucketCounter;
+				bucketCounter = 0;
+				oven.play();
+			}
+
+			if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) paused = true;
+
+		}else{
+			setScene();
+			font.draw(batch, "GAME PAUSED", 320, 240);
+			batch.end();
+			if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) paused = false;
 		}
-		if (levelCounter >= 5 && velocity < 7){
+	}
+
+	private void setScene() {
+		if (backgroundCounter > 6) {
+			if (liveCounter == 3) {
+				randomBackground = MathUtils.random(1, 4);
+				backgroundCounter = 0;
+				backgroundTexture = new Texture(Gdx.files.internal("background" + randomBackground + ".png"));
+				backgroundSprite = new Sprite(backgroundTexture);
+			} else if (liveCounter == 2) {
+				randomBackground = MathUtils.random(1, 3);
+				backgroundCounter = 0;
+				backgroundTexture = new Texture(Gdx.files.internal("twolives" + randomBackground + ".png"));
+				backgroundSprite = new Sprite(backgroundTexture);
+			} else {
+				randomBackground = MathUtils.random(1, 2);
+				backgroundCounter = 0;
+				backgroundTexture = new Texture(Gdx.files.internal("onelive" + randomBackground + ".png"));
+				backgroundSprite = new Sprite(backgroundTexture);
+			}
+		}
+		if (levelCounter >= 5 && velocity < 7) {
 			levelCounter = 5 - levelCounter;
 			breadVelocity += 0.25;
 		}
-		bucketSound = Gdx.audio.newSound(Gdx.files.internal(BucketSound[MathUtils.random(0,2)] +".mp3"));
+		bucketSound = Gdx.audio.newSound(Gdx.files.internal(BucketSound[MathUtils.random(0, 2)] + ".mp3"));
 		ScreenUtils.clear(0, 0, 0.2f, 1);
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 
 		backgroundCounter++;
 		renderBackground();
-		for(Rectangle raindrop: raindrops) {
+		for (Rectangle raindrop : raindrops) {
 			batch.draw(breadImage, raindrop.x, raindrop.y);
 		}
-		for(Rectangle live: lives){
-			batch.draw(liveImage, live.x, live.y);
+		for (Rectangle live : lives) {
+			batch.draw(match, live.x, live.y);
 		}
 
 		batch.draw(breadImage, 460, 420);
+		if (matchBoolean) {
+			batch.draw(match, 460, 360);
+		}
 		font.draw(batch, Integer.toString(breadCounter), 550, 440);
 		if (bucketCounter == 0) {
 			batch.draw(bucketImage, bucket.x, bucket.y);
-		}else if (bucketCounter > 0 && bucketCounter < 5 ){
+		} else if (bucketCounter > 0 && bucketCounter < 5) {
 			batch.draw(bucketImageSemiFull, bucket.x, bucket.y);
-		}else if (bucketCounter > 4 && bucketCounter < 10){
+		} else if (bucketCounter > 4 && bucketCounter < 10) {
 			batch.draw(bucketImageSemiFull2, bucket.x, bucket.y);
-		}else{
+		} else {
 			batch.draw(bucketImageFull, bucket.x, bucket.y);
 		}
 
-		if (liveCounter == 3){
-			batch.draw(liveImage, 40, 420);
-			batch.draw(liveImage, 110, 420);
-			batch.draw(liveImage, 180, 420);
-		}
-		if (liveCounter == 2){
-			batch.draw(liveImage, 40, 420);
-			batch.draw(liveImage, 110, 420);
-			batch.draw(noLiveImage, 180, 420);
-		}
-		if (liveCounter == 1){
-			batch.draw(liveImage, 40, 420);
-			batch.draw(noLiveImage, 110, 420);
-			batch.draw(noLiveImage, 180, 420);
-		}
-
-
-		batch.end();
-
-		if(Gdx.input.isKeyPressed(Input.Keys.LEFT) && !Gdx.input.isKeyPressed(Input.Keys.RIGHT)) xAcceleration = -acceleration;
-		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT) && !Gdx.input.isKeyPressed(Input.Keys.LEFT)) xAcceleration = acceleration;
-		if(!Gdx.input.isKeyPressed(Input.Keys.RIGHT) && !Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-			if (velocity > 0) {
-				xAcceleration = -0.02;
-			} else if (velocity < 0) {
-				xAcceleration = +0.02;
-			}
-		}
-		velocity += xAcceleration;
-		velocity *= 1 - friction;
-		bucket.x += velocity;
-
-		if(bucket.x < 0) bucket.x = 0;
-		if(bucket.x > 645) bucket.x = 645;
-
-		if(TimeUtils.nanoTime() - lastDropTime > 1900000000) spawnBread();
-		if(TimeUtils.nanoTime() - lastDropTimeLive > 1000000000) fallingLives();
-
-
-		for (Iterator<Rectangle> iter = raindrops.iterator(); iter.hasNext(); ) {
-			Rectangle raindrop = iter.next();
-			raindrop.y -= breadVelocity;
-			if(raindrop.y - 20 < 0){
-				iter.remove();
-				liveLostSound.play();
-				liveCounter -= 1;
-				if (liveCounter == 0){
-					Gdx.app.exit();
-				}
-			}
-			if(raindrop.overlaps(bucket)) {
-				if (bucketCounter < 10) {
-					bucketSound.play();
-					bucketCounter++;
-					iter.remove();
-				}else{
-					Gdx.app.exit();
-				}
-			}
-		}
-
-		for (Iterator<Rectangle> iter = lives.iterator(); iter.hasNext(); ) {
-			Rectangle live = iter.next();
-			live.y -= breadVelocity;
-			if(live.y - 20 < 0){
-				iter.remove();
-			}
-			if(live.overlaps(bucket)) {
-				if (liveCounter < 3) {
-					liveSound.play();
-					liveCounter++;
-				}else{
-					bucketSound.play();
-				}
-				iter.remove();
-			}
-		}
-
-		if (bucket.x == 645 && bucketCounter > 0){
-			breadCounter += bucketCounter;
-			levelCounter += bucketCounter;
-			bucketCounter = 0;
-			oven.play();
-		}
-
-
+		if (bucket.x == 645 && matchBoolean) font.draw(batch, "Press 'E' to ignite", 320, 50);
 	}
 
 	@Override
